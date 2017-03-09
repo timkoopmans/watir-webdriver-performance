bb06d6d34ccdab0a9c2659c0cc4e19d4b79c3700
Sam Nissen <scnissen@gmail.com>
2017-03-09 10:06:24 +0000

    Make pending the pending tests

--- spec/watir-webdriver-performance-non-supported-browser_spec.rb
+++ spec/watir-webdriver-performance-non-supported-browser_spec.rb
@@ -2,27 +2,36 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "WatirWebdriverPerformance-NonSupportedBrowser" do
 
-  let!(:b) { @b }
+  # let!(:b) { @b }
 
-  before(:all) do
-    @b ||= Watir::Browser.new :firefox
-  end
+  # before(:all) do
+    # @b ||= Watir::Browser.new :firefox
+  # end
 
-  after(:all) do
-    @b.close if @b
-  end
+  # after(:all) do
+    # @b.close if @b
+  # end
 
-  pending "should raise an error when a non supported browser is encountered" do
+  it "should raise an error when a non supported browser is encountered" do
+    pending("These tests appear to lack one required pre-condition: necessarily creating a browser that is non-performant.")
+    fail
+
     b.goto "google.com"
     lambda {  b.performance }.should raise_error RuntimeError, 'Could not collect performance metrics from your current browser. Please ensure the browser you are using supports collecting performance metrics.'
   end
 
-  pending "should return false for supported" do
+  it "should return false for supported" do
+    pending("These tests appear to lack one required pre-condition: necessarily creating a browser that is non-performant.")
+    fail
+
     b.goto "google.com"
     b.should_not be_performance_supported
   end
 
-  pending "should not support performance as block" do
+  it "should not support performance as block" do
+    pending("These tests appear to lack one required pre-condition: necessarily creating a browser that is non-performant.")
+    fail
+
     b.goto "google.com"
     b.with_performance {|performance| performance.should be_nil }
   end
